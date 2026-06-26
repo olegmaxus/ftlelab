99d0e3103ce382c8d9fe483360b9f2450ad889af
Oleg M <olegmaxus@gmail.com>
2026-06-26 16:02:58 +0200

    Update: remove .viz from __init__.py

--- src/ftlelab/data/__init__.py
+++ src/ftlelab/data/__init__.py
@@ -1,4 +1,3 @@
 from .synthetic import make_circle_dataset, make_moons_dataset, make_spiral_dataset, make_xor_dataset
 from .splits import split_dataset
 from .loaders import make_dataloaders
-from .viz import plot_2d_dataset
